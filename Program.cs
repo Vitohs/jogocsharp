201b5c1f6bc097ad5a27a81c4f8b3c547a9f539a
Donvito <vhugodossantos33@gmail.com>
2024-08-11 20:27:45 -0300

    lancei

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Runtime;
+string controle = "";
+Random chave = new Random();
+var chance = 10;
+var numero = chave.Next(1,100);
+var digitado = 0;
+
+Console.WriteLine("Bem vindo ao jogo de adivinhe, quer jogar ? s/n");
+controle = Console.ReadLine();
+if (controle == "s")
+{
+    do
+    {
+        Console.WriteLine($"selecione o numero, voce tem {chance} chances restante");
+        digitado = Convert.ToInt32(Console.ReadLine());
+        
+        if (digitado < numero)
+        {
+            Console.Clear();
+            Console.WriteLine("numero esta em um valor maior");
+            chance--;
+        }
+        if (digitado > numero)
+        {
+            Console.Clear();
+            Console.WriteLine("numero esta em um valor menor");
+            chance--;
+        }
+        if (digitado == numero)
+        {
+            Console.Clear();
+            Console.WriteLine("Parabens voce acertou");
+            break;
+        }
+        if (chance == 0)
+        {
+            Console.WriteLine($"perdeu pae, o numero era {numero}");
+            break;
+        }
+    }
+    while (true);
+}
+else
+{
+    Console.WriteLine("tchau");
+}
+Console.ReadKey();
